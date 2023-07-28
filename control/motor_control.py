cb804d43ea0921148aa457dad01b29518b1bcb1f
vkolla <vkolla@pps.co.za>
2023-07-28 10:43:29 +0200

    grasp planning and control

---
+++ control/motor_control.py
@@ -0,0 +1,12 @@
+"""
+Grasp Planning Module
+
+This module is responsible for planning how the Litterbot will grasp detected objects.
+It takes as input the type and position of the object, and outputs a series of motor
+commands that will result in the object being grasped.
+
+Functions:
+    plan_grasp(object_type, object_position):
+        Plans a grasp for the given object.
+"""
+
